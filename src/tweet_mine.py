32d2501558a30aed75d4a63f585d8d8f8623892b
Brian In <hibc@naver.com>
2018-03-25 18:12:50 -0700

    assign follow and track None if no contents

--- src/tweet_mine.py
+++ src/tweet_mine.py
@@ -64,8 +64,8 @@ class TweetMine():
         else:
             self.languages=[self.search_language]
         
-        self.search_follow_list = self.search_follow.split(',')    
-        self.search_track_list = self.search_track.split(',')
+        self.search_follow_list = None if self.search_follows == "" else self.search_follow.split(',')    
+        self.search_track_list = None if self.search_track == "" else self.search_track.split(',')
     
     def run(self):
         num_of_tweets = 0
@@ -107,9 +107,9 @@ if __name__ == '__main__':
     parser.add_argument('--file_path', type=str)
     args = parser.parse_args()
 
-    # print("\nconsumer_key: {0}\nconsumer_secret: {1}\naccess_token: {2}\naccess_secret: {3}\n".format(args.consumer_key, args.consumer_secret, args.access_token, args.access_secret))
-    # print("location: {0}\nlaunguages: {1}\nfollow: {2}\ntrack: {3}\n".format(args.locations, args.languages, args.follow, args.track))
-    # print("file_path: {0}\n".format(args.file_path))
+    print("\nconsumer_key: {0}\nconsumer_secret: {1}\naccess_token: {2}\naccess_secret: {3}\n".format(args.consumer_key, args.consumer_secret, args.access_token, args.access_secret))
+    print("location: {0}\nlaunguages: {1}\nfollow: {2}\ntrack: {3}\n".format(args.locations, args.languages, args.follow, args.track))
+    print("file_path: {0}\n".format(args.file_path))
     # sys.exit(0)
     tm = TweetMine(config_args=args)
     tm.run()
